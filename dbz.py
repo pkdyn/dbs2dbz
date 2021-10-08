07e899f0b6437f52252bd3b4cea555cc5643882d
pkdyn <puneetsk21@gmail.com>
2021-10-08 18:58:30 +0530

    integrate heroku3.tfcpu.nonconda

--- dbz.py
+++ dbz.py
@@ -1,11 +1,8 @@
 from __future__ import print_function, division
 import scipy
 import scipy.misc
-from keras.datasets import mnist
 from keras_contrib.layers.normalization.instancenormalization import InstanceNormalization
-#from tensorflow_addons.layers import InstanceNormalization
 from keras.layers import Input, Dense, Reshape, Flatten, Dropout, Concatenate
-from keras.layers import BatchNormalization, Activation, ZeroPadding2D
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.convolutional import UpSampling2D, Conv2D
 from keras.models import Sequential, Model
